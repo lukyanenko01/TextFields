98c94419c4e490f711058cdd5cd16bb3994e5681
Vladimir Lukyanenko <>
2023-02-19 14:39:04 +0200

    first commit

---
+++ TextFieldsSwiftUI/TextFieldsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldsSwiftUIApp.swift
+//  TextFieldsSwiftUI
+//
+//  Created by Vladimir Lukyanenko on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
